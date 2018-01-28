e06a38ee2500c1e86c993a97a6f9efd1c5c220b3
Ryan Spraetz <spraetz@gmail.com>
2018-01-28 13:57:16 -0800

    more config changes

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import os
 
 app = Flask(__name__)
 
+settings = {}
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello {}!'.format(app.config["ENVIRONMENT"])
+    return 'Hello {}!'.format(settings.get("environment", "omg"))
 
 
 if __name__ == '__main__':
-    app.config["ENVIRONMENT"] = os.environ.get("environment", "wtf")
-    app.config["DEBUG"] = True
+    settings["environment"] = os.environ.get("environment", "wtf")
     app.run()
