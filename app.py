8d363190f77609baf4f01a86a495839831bf437b
Ryan Spraetz <spraetz@gmail.com>
2018-01-28 13:46:59 -0800

    environments

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Staging!'
+    return 'Hello {}!'.format(app.config["ENVIRONMENT"])
 
 
 if __name__ == '__main__':
+    app.config["ENVIRONMENT"] = os.environ['environment']
     app.run()
